64bea31d84a532057e43a7c354fb054b21da90d4
Leonardo Ayala <leonardo.ayalao@pucp.edu.pe>
2022-12-04 14:33:41 -0500

    crud objeto terminado

--- app/src/main/java/com/example/loob/admin/AgregarObjetoActivity.java
+++ app/src/main/java/com/example/loob/admin/AgregarObjetoActivity.java
@@ -1,16 +1,175 @@
 package com.example.loob.admin;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.loob.R;
+import com.example.loob.dto.ObjetoDTO;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.Calendar;
 
 public class AgregarObjetoActivity extends AppCompatActivity {
+
+    String otro = "";
+    int anioActual;
+    int mesActual;
+    int diaActual;
+    int anioElegido;
+    int mesElegido;
+    int diaElegido;
+    FirebaseDatabase firebaseDatabase;
+    Uri uri;
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    FirebaseUser user = firebaseAuth.getCurrentUser();
+    FirebaseStorage storage = FirebaseStorage.getInstance();
+    StorageReference storageReference = storage.getReference();
+    ActivityResultLauncher<Intent> launcher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == RESULT_OK){
+                    uri = result.getData().getData();
+                }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_objeto);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = firebaseDatabase.getReference();
+
+        EditText textNombre = findViewById(R.id.textNombreA);
+        EditText textHidden = findViewById(R.id.editTextHidden);
+        String[] listaTipo = {"Dispositivo","Identificacion","Util escolar","Efectivo","Prenda", "Otro"};
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(AgregarObjetoActivity.this, android.R.layout.simple_spinner_dropdown_item,listaTipo);
+        Spinner spinnerTipo = findViewById(R.id.spinnerTipo);
+        spinnerTipo.setAdapter(adapter1);
+        spinnerTipo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String tipo = spinnerTipo.getSelectedItem().toString();
+                if(tipo.equals("Otro")){
+                    textHidden.setEnabled(true);
+                }else{
+                    textHidden.setEnabled(false);
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+        EditText textMarca = findViewById(R.id.textMarcaA);
+        EditText textCaracteristicas = findViewById(R.id.textCaracteristicasA);
+        EditText textFecha = findViewById(R.id.textFechaA);
+        Button btnCalendar = findViewById(R.id.btnCalendar);
+        btnCalendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar calendar = Calendar.getInstance();
+                int anio = calendar.get(Calendar.YEAR);
+                int mes = calendar.get(Calendar.MONTH);
+                int dia = calendar.get(Calendar.DAY_OF_MONTH);
+                diaActual = dia;
+                mesActual = mes;
+                anioActual = anio;
+                DatePickerDialog datePickerDialog = new DatePickerDialog(AgregarObjetoActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                        String fecha = i2 + "/" + (i1+1) + "/" + i;
+                        anioElegido = i;
+                        mesElegido = i1;
+                        diaElegido = i2;
+                        textFecha.setText(fecha);
+                    }
+                },anio,mes,dia);
+                datePickerDialog.show();
+            }
+        });
+        Button btnAgregar = findViewById(R.id.btnAgregarDispositivo);
+        btnAgregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nombre = textNombre.getText().toString();
+                String tipo = spinnerTipo.getSelectedItem().toString();
+                String marca = textMarca.getText().toString();
+                String caracteristicas = textCaracteristicas.getText().toString();
+                String fecha = textFecha.getText().toString();
+                if (nombre.trim().isEmpty()) {
+                    Toast.makeText(AgregarObjetoActivity.this, "Nombre no puede ser vacio", Toast.LENGTH_SHORT).show();
+                } else if (marca.trim().isEmpty()) {
+                    Toast.makeText(AgregarObjetoActivity.this, "Marca no puede ser vacio", Toast.LENGTH_SHORT).show();
+                } else if (caracteristicas.trim().isEmpty()) {
+                    Toast.makeText(AgregarObjetoActivity.this, "Caracteristicas no puede ser vacio", Toast.LENGTH_SHORT).show();
+                } else if (fecha.trim().isEmpty()) {
+                    Toast.makeText(AgregarObjetoActivity.this, "Fecha no puede ser vacio", Toast.LENGTH_SHORT).show();
+                } else if (anioActual<anioElegido) {
+                    Toast.makeText(AgregarObjetoActivity.this, "Fecha no puede ser despues de la actual", Toast.LENGTH_SHORT).show();
+                } else if ((anioActual==anioElegido)&&(mesActual<mesElegido)){
+                    Toast.makeText(AgregarObjetoActivity.this, "Fecha no puede ser despues de la actual", Toast.LENGTH_SHORT).show();
+                } else if ((anioActual==anioElegido)&&(mesActual==mesElegido)&&(diaActual<diaElegido)) {
+                    Toast.makeText(AgregarObjetoActivity.this, "Fecha no puede ser despues de la actual", Toast.LENGTH_SHORT).show();
+                }else if (tipo.equals("Otro")){
+                    otro = textHidden.getText().toString();
+                    if(otro.trim().isEmpty()){
+                        textHidden.setError("No puede ser vacío");
+                        textHidden.requestFocus();
+                    }else{
+                        tipo = otro;
+                        ObjetoDTO objeto = new ObjetoDTO();
+                        objeto.setNombre(nombre);
+                        objeto.setTipo(tipo);
+                        objeto.setMarca(marca);
+                        objeto.setCaracteristicas(caracteristicas);
+                        objeto.setFecha(fecha);
+                        databaseReference.child("objetos").push().setValue(objeto);
+                        Toast.makeText(AgregarObjetoActivity.this, "Objeto agregado correctamente", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(AgregarObjetoActivity.this,ListaObjetosActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }else{
+                    ObjetoDTO objeto = new ObjetoDTO();
+                    objeto.setNombre(nombre);
+                    objeto.setTipo(tipo);
+                    objeto.setMarca(marca);
+                    objeto.setCaracteristicas(caracteristicas);
+                    objeto.setFecha(fecha);
+                    databaseReference.child("objetos").push().setValue(objeto);
+                    Toast.makeText(AgregarObjetoActivity.this, "Objeto agregado correctamente", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(AgregarObjetoActivity.this,ListaObjetosActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        });
+        Button btnCancelar = findViewById(R.id.btnCancelarA);
+        btnCancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AgregarObjetoActivity.this,ListaObjetosActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/loob/admin/EditarObjetoActivity.java
+++ app/src/main/java/com/example/loob/admin/EditarObjetoActivity.java
@@ -2,15 +2,115 @@ package com.example.loob.admin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.loob.R;
+import com.example.loob.dto.ObjetoDTO;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Calendar;
 
 public class EditarObjetoActivity extends AppCompatActivity {
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    int anioActual;
+    int mesActual;
+    int diaActual;
+    int anioElegido;
+    int mesElegido;
+    int diaElegido;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar_objeto);
+        Intent intent = getIntent();
+        ObjetoDTO objeto = (ObjetoDTO) intent.getSerializableExtra("objeto");
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+
+        EditText textMarca = findViewById(R.id.textMarcaEdit);
+        textMarca.setText(objeto.getMarca());
+        EditText textCaracteristicas = findViewById(R.id.textCaracteristicasEdit);
+        textCaracteristicas.setText(objeto.getCaracteristicas());
+        EditText textFecha = findViewById(R.id.textFechaE);
+        textFecha.setText(objeto.getFecha());
+        textFecha.setEnabled(false);
+
+        Button btnCalendar = findViewById(R.id.btnCalendarE);
+        btnCalendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar calendar = Calendar.getInstance();
+                int anio = calendar.get(Calendar.YEAR);
+                int mes = calendar.get(Calendar.MONTH);
+                int dia = calendar.get(Calendar.DAY_OF_MONTH);
+                diaActual = dia;
+                mesActual = mes;
+                anioActual = anio;
+                DatePickerDialog datePickerDialog = new DatePickerDialog(EditarObjetoActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                        String fecha = i2 + "/" + (i1+1) + "/" + i;
+                        anioElegido = i;
+                        mesElegido = i1;
+                        diaElegido = i2;
+                        textFecha.setText(fecha);
+                    }
+                },anio,mes,dia);
+                datePickerDialog.show();
+            }
+        });
+
+        Button btnEditar = findViewById(R.id.btnEdit);
+        btnEditar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String marca = textMarca.getText().toString();
+                String caracteristicas = textCaracteristicas.getText().toString();
+                String fecha = textFecha.getText().toString();
+                if (marca.trim().isEmpty()) {
+                    Toast.makeText(EditarObjetoActivity.this, "Marca no puede ser vacio", Toast.LENGTH_SHORT).show();
+                } else if (caracteristicas.trim().isEmpty()) {
+                    Toast.makeText(EditarObjetoActivity.this, "Caracteristicas no puede ser vacio", Toast.LENGTH_SHORT).show();
+                } else if (fecha.trim().isEmpty()) {
+                    Toast.makeText(EditarObjetoActivity.this, "Incluye no puede ser vacio", Toast.LENGTH_SHORT).show();
+                } else if (anioActual<anioElegido) {
+                    Toast.makeText(EditarObjetoActivity.this, "Fecha no puede ser despues de la actual", Toast.LENGTH_SHORT).show();
+                } else if ((anioActual==anioElegido)&&(mesActual<mesElegido)){
+                    Toast.makeText(EditarObjetoActivity.this, "Fecha no puede ser despues de la actual", Toast.LENGTH_SHORT).show();
+                } else if ((anioActual==anioElegido)&&(mesActual==mesElegido)&&(diaActual<diaElegido)) {
+                    Toast.makeText(EditarObjetoActivity.this, "Fecha no puede ser despues de la actual", Toast.LENGTH_SHORT).show();
+                } else {
+                    objeto.setMarca(marca);
+                    objeto.setCaracteristicas(caracteristicas);
+                    objeto.setFecha(fecha);
+                    databaseReference.child("objetos").child(objeto.getId()).setValue(objeto);
+                    Toast.makeText(EditarObjetoActivity.this, "Objeto editado correctamente", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(EditarObjetoActivity.this,ListaObjetosActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        });
+
+        Button btnCancelar = findViewById(R.id.btnCancelarEdit);
+        btnCancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(EditarObjetoActivity.this,ListaObjetosActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
